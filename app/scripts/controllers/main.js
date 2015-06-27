aac312887fe5ea71af34c91faaa02a00e7dbe9ff
Pavan Yerraboina <pavanyerraboina@gmail.com>
2015-06-27 10:14:20 +0100

    merged the conflicts

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -18,6 +18,6 @@ angular.module('newFaceAngularApp')
     $scope.iframes = [	{id: 6773615},{id: 6773615},{id: 6773615},{id: 6773615} ,{id: 6773615}  ];
 
     $scope.getIframeSrc = function(id) {
-	  return 'http://flashservice.xvideos.com/embedframe/' + id;
-	};
+  	  return 'http://flashservice.xvideos.com/embedframe/' + id;
+  	};
   });
